e00f41a6503f1f7275b164eb2edc09671a33a268
Skander <skander.kc@gmail.com>
2018-01-19 12:22:12 -0500

    Calculate winner

--- src/index.js
+++ src/index.js
@@ -62,6 +62,11 @@ class Board extends Component {
   */
   handleClick(i) {
     const squaresCopy = this.state.squares.slice(); // Immutability
+
+    if (calculateWinner(squaresCopy) || squaresCopy[i]) {
+      return;
+    }
+
     squaresCopy[i] = this.state.xIsNext ? 'x' : 'o';
     this.setState({
       squares: squaresCopy,
@@ -70,7 +75,14 @@ class Board extends Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'x' : 'o');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'x' : 'o');
+    }
+
 
     return (
       <div>
@@ -111,9 +123,34 @@ class Game extends Component {
   }
 }
 
+// Helpers
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
 // ========================================
 
 ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+
+
