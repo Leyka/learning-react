10fd7013dd5212d42fc8ebebb85658bc9be15a9f
Skander <skander.kc@gmail.com>
2018-01-23 13:30:32 -0500

    React-redux init

---
+++ react-redux/src/containers/Counter/Counter.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Counter extends Component {
+  render() {
+    return (
+      <h1> My Counter </h1>
+    );
+  }
+}
+
+export default Counter
