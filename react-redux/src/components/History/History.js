d0a07e410da32af5f2ee1d714936c16fc67596bc
Skander <skander.kc@gmail.com>
2018-01-24 14:03:06 -0500

    Display size of history

--- react-redux/src/components/History/History.js
+++ react-redux/src/components/History/History.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 const history = props => {
   const history = props.history;
-  const listHistory = history.map( action =>
-    <div>{action.initial} {action.type === 'ADD' ? '+' : '-'} {action.value} = {action.result}</div>
+  const listHistory = history.map( (action, index) =>
+    <p key={'action_' + index}>
+      {action.initial} {action.type === 'ADD' ? '+' : '-'} {action.value} = {action.result}
+    </p>
   );
 
   return (
-    <div className="history">
-      <h3>List of actions</h3>
+    <div>
+      <small>-- History Component --</small>
+      <h3>List of {listHistory.length} action(s)</h3>
       <pre>{listHistory}</pre>
     </div>
   );
